24c30b95b21fbca0076c62682600ea81f689778c
Imozuro <kozlovt873@gmail.com>
2025-04-29 20:14:00 +0300

    add 2 block, create JS folder and JS file

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Функция для переключения слайдов
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    // Цикличность
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    // Сдвиг слайдов
+    let slidesContainer = document.querySelector('.slides');
+    slidesContainer.style.transform = `translateX(-${(slideIndex - 1) * 100}%)`;
+}
